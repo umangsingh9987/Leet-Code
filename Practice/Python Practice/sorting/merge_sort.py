149d4b9e6020e0d6a2f081a114b883585359c8cd
Umang Singh <choudharyumangjatt9987@gmail.com>
2025-01-05 16:51:26 +0530

    sorting alogs

---
+++ Practice/Python Practice/sorting/merge_sort.py
@@ -0,0 +1,53 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Finding the mid of the array
+        mid = len(arr) // 2
+
+        # Dividing the array elements into 2 halves
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort the first half
+        merge_sort(left_half)
+
+        # Recursively sort the second half
+        merge_sort(right_half)
+
+        # Initialize pointers for left_half, right_half, and the main array
+        i = j = k = 0
+
+        # Copy data to temp arrays left_half[] and right_half[]
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left in left_half
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        # Checking if any element was left in right_half
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+def print_list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+# Driver code to test the above
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    print_list(arr)
+    merge_sort(arr)
+    print("Sorted array is: ", end="\n")
+    print_list(arr)
